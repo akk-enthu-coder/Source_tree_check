9de612e2fdff83b6aef4196da09eee3e535cb956
Akkash Rao <>
2021-09-02 20:02:05 +0530

    initial commit

---
+++ STcheck/STcheck/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  STcheck
+//
+//  Created by Akkash Rao on 02/09/21.
+//
+
+import UIKit
+
+import Foundation
+
+
+This is to check SourceTree
+
---
+++ STcheck/STcheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STcheck
+//
+//  Created by Akkash Rao on 02/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
